a862aa990cd8bc4c02d4e2301c56d04a494148c4
micke-a <mail.micke@gmail.com>
2014-12-14 16:12:00 +0000

    '.'

---
+++ gradle-groovy/script.groovy
@@ -0,0 +1,14 @@
+import micke.Micke
+import micke.Point
+
+/**
+ * Created by User on 13/12/2014.
+ */
+
+Micke m = new Micke()
+
+m.str = "dude"
+
+m.yello()
+
+println new Point(3,5)
---
+++ gradle-groovy/src/main/groovy/micke/Micke.groovy
@@ -0,0 +1,15 @@
+package micke
+
+/**
+ * Created by User on 13/12/2014.
+ */
+class Micke {
+    String str
+
+
+    def yello(){
+        println "yello ${str}"
+
+    }
+
+}
